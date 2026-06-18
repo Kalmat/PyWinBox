368710ce909f53cb7b090d810042f9ba9f266ebe
Kalmat <palookjones@gmail.com>
2026-06-18 14:49:10 +0200

    Fixconf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,24 +6,34 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import re
+import time
+
 project = 'PyWinBox'
-copyright = '2026, Kalmat'
+year = time.strftime("%Y")
 author = 'Kalmat'
-release = 'latest'
+copyright = year + ", " + author
+release = "latest"
+with open("../../src/pywinbox/__init__.py", "r") as fileObj:
+    match = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fileObj.read(), re.MULTILINE
+    )
+    if match:
+        release = match.group(1)
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = []
-
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 templates_path = ['_templates']
-exclude_patterns = []
-
-
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'bizstyle'
 html_static_path = ['_static']
-
